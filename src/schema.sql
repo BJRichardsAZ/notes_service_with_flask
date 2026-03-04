670dccbd5f93a9dc06b9c8766c77d5aeb039bb8b
Bryce Richards <brycejacobrichards@gmail.com>
2026-03-04 13:31:02 -0700

    fix schema, auto increment is improper syntax and not needed

--- src/schema.sql
+++ src/schema.sql
@@ -1,6 +1,9 @@
  -- notes table schema
+ -- existing table should be dropped if init_db is invoked to give a fresh instance
+ DROP TABLE IF EXISTS notes;
+
  CREATE TABLE IF NOT EXISTS notes (
-        id INTEGER AUTO_INCREMENT PRIMARY KEY,
+       id INTEGER PRIMARY KEY,
         content VARCHAR(250) NOT NULL,
         createdAt timestamp DEFAULT CURRENT_TIMESTAMP
         )
