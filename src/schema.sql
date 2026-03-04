2cb43a1bb53a43495d2c44747deb383240c2ea35
Bryce Richards <brycejacobrichards@gmail.com>
2026-03-04 00:00:30 -0700

    add create table if it doesn't exist sql

--- src/schema.sql
+++ src/schema.sql
@@ -1,5 +1,5 @@
  -- notes table schema
- CREATE TABLE NOTES (
+ CREATE TABLE IF NOT EXISTS NOTES (
         id INT AUTO_INCREMENT PRIMARY KEY,
         content VARCHAR(250) NOT NULL,
         createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
