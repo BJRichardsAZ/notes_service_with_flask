31757429c28c7b246f2483bc8b94650d68cb4b50
Bryce Richards <brycejacobrichards@gmail.com>
2026-03-04 00:08:51 -0700

    fix syntax error in schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,6 +1,6 @@
  -- notes table schema
- CREATE TABLE IF NOT EXISTS NOTES (
-        id INT AUTO_INCREMENT PRIMARY KEY,
+ CREATE TABLE IF NOT EXISTS notes (
+        id INTEGER AUTO_INCREMENT PRIMARY KEY,
         content VARCHAR(250) NOT NULL,
-        createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
+        createdAt timestamp DEFAULT CURRENT_TIMESTAMP
         )
