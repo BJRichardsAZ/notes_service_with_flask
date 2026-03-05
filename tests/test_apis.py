75dbc4c3726f65052d15c5d24f0598bb6abb92b2
Bryce Richards <brycejacobrichards@gmail.com>
2026-03-05 10:44:14 -0700

    fix incorrect fixture structure, add missing yield statement

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -1,13 +1,22 @@
 import pytest
 import sqlite3
-from app import create_app
+from app import create_app, init_db
 
 #create database fixture utilizing system memory 
 @pytest.fixture
 def client():
+    # get flask app instance, set storage to system memory 
     app = create_app()
     app.config['TESTING'] = True
     app.config['DATABASE'] = ':memory:'
+    #create flask request environment 
+    with app.app_context():
+        init_db(),
+    #used built in flask method to create a test "browser" instance for requests
+    with app.test_client() as client:
+    #run everything up to yield, tear it down when the test completes
+        yield client
+
     return client; 
 #create parameter variations for endpoint tests (cut down on function code)
 @pytest.mark.parametrize(
