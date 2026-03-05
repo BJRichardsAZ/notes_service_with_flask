d62fe46182383c98262ba4ab853dfdea3cf756c9
Bryce Richards <brycejacobrichards@gmail.com>
2026-03-05 08:22:26 -0700

    add class and error type for string length being too long

--- src/errors.py
+++ src/errors.py
@@ -17,6 +17,12 @@ class MissingParameter(Exception):
         super().__init__()
         self.message = message
 
+class InvalidNoteLength(Exception): 
+    status_code = 422 
+
+    def __init__(self, message= "Content parameter was over the 250 character limit."):
+        super().__init__()
+        self.message = message
 
 
 #Error handlers
@@ -29,6 +35,7 @@ def register_error_handlers(app):
             "error": str(e)
         }), e.status_code
     
+    #custom handler for missing parameters in request
     @app.errorhandler(MissingParameter)
     def handle_missing_parameter(e):    
         return jsonify({
@@ -43,6 +50,14 @@ def register_error_handlers(app):
             "error": "Not Found."
         }), 404
     
+    #custom handler for content being to long
+    @app.errorhandler(InvalidNoteLength)
+    def handle_invalid_note_length(e):
+        return jsonify({
+            "success": False,
+            "error": str(e)
+        }), 422
+    
     #catch all handler for http errors 
     @app.errorhandler(HTTPException)
     def handle_http_exception(e):
@@ -51,7 +66,7 @@ def register_error_handlers(app):
             "error": e.description
         }), e.code
     
-    #catch all for other exceptions
+    #catch all handler for other exceptions
     @app.errorhandler(Exception)
     def handle_generic_exception(e):
         return jsonify({
