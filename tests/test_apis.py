cdc2292d1fc412b4d2611118c365e4b64a0c053c
Bryce Richards <brycejacobrichards@gmail.com>
2026-03-06 13:52:32 -0700

    add new endpoint tests for individual grabs; remove unneeded method parameter

--- tests/test_apis.py
+++ tests/test_apis.py
@@ -1,6 +1,6 @@
 import pytest
 from src.app import create_app
-from src.db import init_db, get_db
+from src.db import init_db, get_db, close_db
 
 
 # create database fixture utilizing system memory 
@@ -19,6 +19,9 @@ def client():
         # Create the test client INSIDE the same context/connection
         with app.test_client() as c:
             yield c
+            
+    with app.app_context():
+        close_db()
 
 
 # fixture for GET tests that need data in the DB — reuses same client
@@ -34,22 +37,21 @@ def client_with_notes(client):
 
 # create parameter variations for endpoint tests (cut down on function code)
 @pytest.mark.parametrize(
-    "method, endpoint, data, expected_status, expected_success, expected_message, expected_notes",
+    "endpoint, data, expected_status, expected_success, expected_message, expected_notes",
     [
          #POST when content is not provided, should result in a 400
-        ("post", "/notes", None, 400, False, 'Missing required parameter: content.', None),
+        ("/notes", None, 400, False, 'Missing required parameter: content.', None),
          #POST when content is  left empty; should result in 400 error
-        ("post", "/notes", {"content": ""}, 400, False, 'Missing required parameter: content.', None),
+        ("/notes", {"content": ""}, 400, False, 'Missing required parameter: content.', None),
         #POST string entered for content is longer than 250 characters, should throw an error
-        ("post", "/notes", {"content": "a"*251}, 422, False, "Content parameter was over the 250 character limit.", None),
+        ("/notes", {"content": "a"*251}, 422, False, "Content parameter was over the 250 character limit.", None),
         #POST when content parameter is provided, note should post succesfully 
-        ("post", "/notes", {"content": "test"}, 200, True, "Note added succesfully!", None),
+        ("/notes", {"content": "test"}, 200, True, "Note added succesfully!", None),
     ],
 )
 #create test function
-def test_notes_endpoint_post(client, method, endpoint, data, expected_status, expected_success, expected_message, expected_notes):
-    if method == "post":
-        response = client.post(endpoint, data=data or {})
+def test_notes_endpoint_post(client, endpoint, data, expected_status, expected_success, expected_message, expected_notes):
+    response = client.post(endpoint, data=data or {})
     json_data = response.get_json()
 
     assert response.status_code == expected_status
@@ -60,19 +62,28 @@ def test_notes_endpoint_post(client, method, endpoint, data, expected_status, ex
 
 # GET tests use fixture with data and without
 @pytest.mark.parametrize(
-    "expected_status, expected_success, expected_message, expected_notes",
+    "endpoint, expected_status, expected_success, expected_message, expected_notes",
     [
         #GET when db is empty; should result in success with empty array 
-        (200, True, "Notes grabbed succesfully!", []),
+        ("/notes", 200, True, "Notes grabbed succesfully!", []),
         #GET when db is not empty, should result in 200 and display list
-        (200, True, "Notes grabbed succesfully!", [
+        ("/notes", 200, True, "Notes grabbed succesfully!", [
             {"content": "Need new shoes.", "id": 1},
             {"content": "Hello how are you?.", "id": 2}
         ]),
+        #GET single note by id
+        ("/notes/1", 200, True, "Note grabbed succesfully!", 
+         [{"content": "Need new shoes.", "id": 1}]),
+        #GET single note by but id doesn't exist
+        ("/notes/45", 404, False, "Note with that id not found.", 
+         [{}]),
+          #GET single note by but id wasn't passed in request
+        ("/notes/", 404, False, "Not Found", 
+         [{}]),
     ],
 )
-def test_notes_endpoint_get(client_with_notes, expected_status, expected_success, expected_message, expected_notes):
-    response = client_with_notes.get("/notes")
+def test_notes_endpoint_get(client_with_notes, endpoint, expected_status, expected_success, expected_message, expected_notes):
+    response = client_with_notes.get(endpoint)
     
     print(response)
     json_data = response.get_json()
@@ -81,3 +92,4 @@ def test_notes_endpoint_get(client_with_notes, expected_status, expected_success
     assert json_data["success"] == expected_success
     actual_text = str(json_data.get("error") or json_data.get("message", ""))
     assert expected_message in actual_text
+
