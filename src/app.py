dea73387ebf91dfea9aa5f358d7593c1f3079dd5
Bryce Richards <brycejacobrichards@gmail.com>
2026-03-07 13:43:23 -0700

    only fallback to hardcoded db path if no env is present for docker volume

--- src/app.py
+++ src/app.py
@@ -7,12 +7,11 @@ import os
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    #create a reference to the db in config for ease
-    app.config['DATABASE'] = os.path.join(app.root_path, 'notes.db')
-
+    #needed to change this so that in docker data is actually saved in the volume, outside of test enviroment we grab env variable for path
     if test_config is not None:
         app.config.update(test_config)
-
+    else:
+        app.config['DATABASE'] = os.getenv('DATABASE', os.path.join(app.root_path, 'notes.db'))
     register_error_handlers(app)
 
     app.register_blueprint(bp)
@@ -22,10 +21,10 @@ def create_app(test_config=None):
     return app
 
 
-# Create the global app instance for running directly
+#create the global app instance for running directly
 app = create_app()
 
-# write a cli command for initializing (or recreating fresh) the tables from schema file 
+#write a cli command for initializing (or recreating fresh) the tables from schema file 
 from src.db import init_db
 @app.cli.command('init_db')
 def init_db_command():
